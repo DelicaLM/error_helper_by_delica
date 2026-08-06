2fa644fcf61b5ce505864ef089bcbd24599c94c0
delicalm <stormindustries22@outlook.com>
2026-08-06 14:29:48 -0500

    Updating error library

--- error_helper_by_delica/error_helper_funcs.py
+++ error_helper_by_delica/error_helper_funcs.py
@@ -98,6 +98,63 @@ def check_type(input_value, required_type, var_name, alt_type=None, alt_types=No
     # if there was no need for a TypeError).
     return is_correct_type
 
+def check_list_item_types(input_list, required_item_type, var_name, alt_type=None, alt_types=None, prefix = ""):
+    """Checks if all items in a list have the correct type. Raises a TypeError with an informative error message if
+    any of the items in the list have an invalid type.
+
+    Parameters
+    ----------
+    input_list : list
+        The list of items to check.
+    required_item_type : type
+        The type that every item in the list should have.
+    var_name : str
+        The name of the list variable that is being checked (necessary for creating detailed error messages).
+    alt_type : type, default None
+        An alternative valid type that the list items can have, if there is one.
+    alt_types : list[type], default None
+        A list of alternative valid types, if there are more than one.
+    prefix : str, default ""
+        A string prefix for the error message if one is raised (empty string by default).
+
+    Returns
+    -------
+    all_items_have_correct_type : bool
+        Boolean for whether all of the list items have the correct type or a valid alternative type.
+
+    Raises
+    ------
+    TypeError
+        Raised if any of the list items have an incorrect type (or if any of the parameters for this function have an
+        incorrect type).
+    """
+    check_type(input_list, list, "Input List (in check list item types)")
+    check_type(required_item_type, type, "Required Item Type (in check list item types)")
+    check_type(var_name, str, "Var Name (in check list item types)")
+    if alt_type is not None:
+        check_type(alt_type, type, "Alternative Type (in check list item types)")
+    if alt_types is not None:
+        check_type(alt_types, list, "Alternative Types List (in check list item types)")
+    check_type(prefix, str, "Prefix String (in check list item types)")
+    all_items_have_correct_type = True
+    item_index = 0
+    list_length = len(input_list)
+    while item_index < list_length and all_items_have_correct_type:
+        item = input_list[item_index]
+        item_type = type(item)
+        item_has_correct_type = item_type == required_item_type
+        if not item_has_correct_type and alt_type is not None:
+            item_has_correct_type = item_type == alt_type
+        if not item_has_correct_type and alt_types is not None:
+            alt_types_length = len(alt_types)
+            alt_types_index = 0
+            while alt_types_index < alt_types_length and not item_has_correct_type:
+                item_has_correct_type = item_type == alt_types[alt_types_index]
+                alt_types_index += 1
+        all_items_have_correct_type &= item_has_correct_type
+        item_index += 1
+    return all_items_have_correct_type
+
 def check_can_convert(input_value, post_convert_type, var_name, prefix=""):
     """Checks if an input value can be converted to a desired type and raises a TypeError if not.
 
